35de8813ae57aec6d35d9ab48ab2c69eed707f7e
StanislawJakubowski <stanislaw.jakubowski@o2.pl>
2020-02-17 10:31:15 +0100

    ThreadExecutor

--- src/main/java/example/ThreadExecutor.java
+++ src/main/java/example/ThreadExecutor.java
@@ -1,4 +1,22 @@
 package example;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadExecutor {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(() -> steps());
+        executorService.submit(() -> steps());
+
+        executorService.shutdown();     //close of thread pool
+
+    }
+
+    private static void steps(){
+        for (int i = 0; i <999 ; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i );
+        }
+    }
 }
